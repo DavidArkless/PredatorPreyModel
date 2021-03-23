f46963825f0f3b182a6b1bb4048e5b110e2daeed
David <david.arkless@btinternet.com>
2021-03-23 20:34:45 +0000

    N/A

--- modelparams.py
+++ modelparams.py
@@ -1,29 +1,34 @@
 import numpy as np
 
 
-UPTAKE_VELOCITY = 5
-SATURATION_CONSTANT = 1.5
-YIELD = 10
 
+K_PREY = 1500
+PREY_GROWTH = 0.02
+PREY_DECAY = 0.05
+
+K_PREDATOR = 46
+PREDATOR_GROWTH = 0.05
+PREDATOR_DECAY = 0.01
 
 
 parameters = {
-    "velocity": UPTAKE_VELOCITY,  # Maximum number of prey
-    "K": SATURATION_CONSTANT,  # Maximum number of predators
-    "yield": YIELD, # Reproduction rate of prey
+    "prey_max": K_PREY,  # Maximum number of prey
+    "predator_max": K_PREDATOR,  # Maximum number of predators
+    "prey_growth": PREY_GROWTH, # Reproduction rate of prey
+    "predator_growth": PREDATOR_GROWTH, # Reproduction rate of predator
+    "prey_decay": PREY_DECAY,
+    "predator_decay": PREDATOR_DECAY,
 
 }
 
 """
-Jacob-Monod Model
+Model Combination for periods of different times
 
-dxdt = Vy / (K + y)
+So when population of prey and predator are low
+We use a logistic growth model
 
-C = x + Yield * y
-
-dxdt = V(C - x) / (YK + (C- x))
-
-dydt = -1/Y * Vy / (K + y)
+When both populations are high / at max size we use another set of equations to model the 
+dramatic decrease
 """
 
 initial_values = [[400, 28]]
@@ -33,16 +38,24 @@ time = np.linspace(0, 100, num=1000)
 
 def dxdt(prey_pop, predator_pop, params):
 
-    C = prey_pop + params['yield'] * predator_pop
-
-    prey = (params['velocity'] * (C - prey_pop)) / (params['K'] + (C - prey_pop))
-
+    logistic_growth = (params['prey_growth'] * prey_pop *
+                       (1 - prey_pop / (params['prey_max'] - predator_pop)))
+    prey = logistic_growth
 
     return prey
 
 
 def dydt(prey_pop, predator_pop, params):
 
-    predator = -1 / params['yield'] * (params['velocity'] * predator_pop) / (params['K'] + predator_pop)
+    logistic_growth = (params['predator_growth'] * predator_pop *
+                       (1 - predator_pop / (params['predator_max'] - prey_pop)))
 
-    return predator
+    # logistic_growth_rounded = np.round_(logistic_growth, decimals=2)
+    #
+    # if logistic_growth_rounded != 0:
+    #     prey = logistic_growth
+    #     return prey
+
+    prey = logistic_growth
+
+    return prey
