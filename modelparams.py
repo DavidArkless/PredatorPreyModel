e0cf93390f1b95b538774c46a17c09d7d60affb2
David <david.arkless@btinternet.com>
2021-03-24 13:59:01 +0000

    Mix of logistic growth and lotka-volttera

--- modelparams.py
+++ modelparams.py
@@ -2,33 +2,38 @@ import numpy as np
 
 
 
-K_PREY = 1500
-PREY_GROWTH = 0.02
-PREY_DECAY = 0.05
+K_PREY = 1500 # max prey
+# K_PREDATOR = 46 # max predator
+ALPHA = 1 # prey reproduction rate
+BETA = 1 # predation rate
+GAMMA = 0.2 # predator reproduction rate
+DELTA = 0.3 # predator competition rate
+B = 5 # controls how rapidly an individual predator becomes full
 
-K_PREDATOR = 46
-PREDATOR_GROWTH = 0.05
-PREDATOR_DECAY = 0.01
+
+
+
 
 
 parameters = {
     "prey_max": K_PREY,  # Maximum number of prey
-    "predator_max": K_PREDATOR,  # Maximum number of predators
-    "prey_growth": PREY_GROWTH, # Reproduction rate of prey
-    "predator_growth": PREDATOR_GROWTH, # Reproduction rate of predator
-    "prey_decay": PREY_DECAY,
-    "predator_decay": PREDATOR_DECAY,
+    # "predator_max": K_PREDATOR,  # Maximum number of predators
+    "alpha": ALPHA,
+    "beta": BETA,
+    "gamma": GAMMA,
+    "delta": DELTA,
+    "b": B,
 
 }
 
 """
-Model Combination for periods of different times
+Rosenzweig and MacArthur model
+https://www.cs.bham.ac.uk/~szh/teaching/matlabmodeling/Lecture6_body.pdf
 
-So when population of prey and predator are low
-We use a logistic growth model
+dx/dt = x(A(1 - x/K)- B y/(b+x))
+dy/dt = -y(G - D(x/(b+x)))
 
-When both populations are high / at max size we use another set of equations to model the 
-dramatic decrease
+
 """
 
 initial_values = [[400, 28]]
@@ -38,24 +43,19 @@ time = np.linspace(0, 100, num=1000)
 
 def dxdt(prey_pop, predator_pop, params):
 
-    logistic_growth = (params['prey_growth'] * prey_pop *
-                       (1 - prey_pop / (params['prey_max'] - predator_pop)))
-    prey = logistic_growth
+    prey = prey_pop * (params['alpha'] * (1 - prey_pop / params['prey_max']) -
+                       params['beta'] * predator_pop / (params['b'] + prey_pop))
+
+
+
 
     return prey
 
 
 def dydt(prey_pop, predator_pop, params):
 
-    logistic_growth = (params['predator_growth'] * predator_pop *
-                       (1 - predator_pop / (params['predator_max'] - prey_pop)))
+    predator = -predator_pop * (params['gamma'] - params['delta'] * prey_pop/(params['b'] + prey_pop))
 
-    # logistic_growth_rounded = np.round_(logistic_growth, decimals=2)
-    #
-    # if logistic_growth_rounded != 0:
-    #     prey = logistic_growth
-    #     return prey
 
-    prey = logistic_growth
 
-    return prey
+    return predator
